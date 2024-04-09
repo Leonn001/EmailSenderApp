c9c205e99d990b3661a16b4c7634842c716b2874
Leon <leon.moreira124@gmail.com>
2024-04-08 23:21:28 -0300

    adicionando melhorias de escabilidade e seguranca de erros ao codigo e comentando o codigo

--- src/main/java/br/ucsal/edu/componentemail/factory/EmailFactory.java
+++ src/main/java/br/ucsal/edu/componentemail/factory/EmailFactory.java
@@ -2,29 +2,37 @@ package br.ucsal.edu.componentemail.factory;
 
 import br.ucsal.edu.componentemail.model.Email;
 
-public class EmailFactory {
+import java.util.List;
 
-    public Email createRegisterEmail(String emailFrom, String passwordEmailFrom, String emailTo) {
+/*
+Esta classe serve para criar emails com assuntos e corpo de texto prontos
+Aqui você vai instânciar uma classe Email e os atributos (emailFrom,passwordEmailFrom e emailTo) vão ser passados
+por parametro
+Esta classe não será instanciada, apenas serve para chamar os métodos que instanciar Emails.
+*/
+public abstract class  EmailFactory {
+
+    public static Email createRegisterEmail(String emailFrom, String passwordEmailFrom, List<String> emailTo) {
         String subject = "Confirmação de Cadastro";
         String messageBody = "Olá, Obrigado por se cadastrar! Atenciosamente, EmailSender";
         return new Email(emailFrom,passwordEmailFrom,emailTo, subject, messageBody);
     }
 
-    public Email createPasswordRecoveryEmail(String emailFrom, String passwordEmailFrom, String emailTo) {
+    public static Email createPasswordRecoveryEmail(String emailFrom, String passwordEmailFrom, List<String> emailTo) {
         String subject = "Recuperação de Senha";
         String messageBody = "Olá, Você solicitou a recuperação de sua senha! Atenciosamente, EmailSender";
-        return new Email(emailTo, subject, messageBody);
+        return new Email(emailFrom,passwordEmailFrom,emailTo, subject, messageBody);
     }
 
-    public Email createAccountUpdateEmail(String emailFrom, String passwordEmailFrom, String emailTo) {
+    public static Email createAccountUpdateEmail(String emailFrom, String passwordEmailFrom, List<String> emailTo) {
         String subject = "Atualização de Conta";
         String messageBody = "Olá, Suas informações de conta foram atualizadas com sucesso! Atenciosamente, EmailSender";
-        return new Email(emailTo, subject, messageBody);
+        return new Email(emailFrom,passwordEmailFrom,emailTo, subject, messageBody);
     }
 
-    public Email createOrderNotificationEmail(String emailFrom, String passwordEmailFrom, String emailTo) {
+    public static Email createOrderNotificationEmail(String emailFrom, String passwordEmailFrom, List<String> emailTo) {
         String subject = "Confirmação de Pedido";
         String messageBody = "Olá, Seu pedido foi recebido com sucesso! Atenciosamente, EmailSender";
-        return new Email(emailTo, subject, messageBody);
+        return new Email(emailFrom,passwordEmailFrom,emailTo, subject, messageBody);
     }
 }
--- src/main/java/br/ucsal/edu/componentemail/model/Email.java
+++ src/main/java/br/ucsal/edu/componentemail/model/Email.java
@@ -4,29 +4,54 @@ import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+/*
+A classe de Email serve para ser um modelo de email, os atributos dessa classe representam o que é necessário para um
+email ser enviado, como destinatários, remetente, assunto, corpo de mensagem, a senha de APP do google
+*/
 public class Email {
-    private  String emailFrom;
-    private  String passwordEmailFrom;
-    private  String emailTo;
-    private  String subject;
-    private  String messageBody;
+    private String emailFrom;
+    private String passwordEmailFrom;
+    private List<String> emailsTo = new ArrayList<>();
+    private String subject;
+    private String messageBody;
+    // Aqui nós criamos uma thread para agilizar o envio de multiplos emails
+    private ExecutorService executor = Executors.newFixedThreadPool(10);
 
-    public void sendEmail() {
-        try {
-            Message message = new MimeMessage(getEmailSession());
-            message.setFrom(new InternetAddress(emailFrom));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailTo));
-            message.setSubject(subject);
-            message.setText(messageBody);
-            Transport.send(message);
-            System.out.println("Email enviado com sucesso para " + emailTo);
-        } catch (MessagingException e) {
-            System.out.println("Erro ao enviar o email: " + e.getMessage());
+    /*
+    Este método realiza o envio de emails criando a mensagem com os atributos da classe email
+    e utilizando a API jakarta.mail para enviar os emails.
+    */
+    public void sendEmails() {
+        for (String currentEmail : emailsTo) {
+            executor.execute(() -> {
+                try {
+                    Message message = new MimeMessage(getEmailSession());
+                    message.setFrom(new InternetAddress(emailFrom));
+                    message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(currentEmail));
+                    message.setSubject(subject);
+                    message.setText(messageBody);
+                    Transport.send(message);
+                    System.out.println("Email enviado com sucesso para " + currentEmail);
+                } catch (MessagingException e) {
+                    System.out.println("Erro ao enviar o email para " + currentEmail + ": " + e.getMessage());
+                }
+            });
         }
+        executor.shutdown();
     }
 
+    /*
+    Este método cria e retorna uma instância da classe Session do JavaMail, que é usada para configurar a sessão
+    de e-mail para enviar e-mails usando um servidor SMTP do Gmail. Ele usa as propriedades específicas do Gmail
+    e uma instância de Authenticator para autenticação com o servidor SMTP do Gmail, usando o endereço de e-mail
+    e a senha fornecidos.
+    */
     private Session getEmailSession(){
         return Session.getInstance(getGmailProperties(), new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -35,6 +60,10 @@ public class Email {
         });
     }
 
+    /*
+    Este método retorna um objeto Properties que contém as configurações específicas do servidor SMTP do Gmail
+    para enviar e-mails usando JavaMail.
+    */
     private static Properties getGmailProperties() {
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", "true");
@@ -45,30 +74,71 @@ public class Email {
         return prop;
     }
 
+    /*
+    Aqui nós temos um construtor vazio, como nada é informado ele tem uma mensagem padrão que envia um email
+    do remetente de testes para o destinatário email de testes.
+     */
     public Email() {
-        this.emailTo = "emailsendert82@gmail.com";
+        this.emailsTo.add("emailsendert82@gmail.com");
         this.emailFrom = "emailsendert82@gmail.com";
         this.passwordEmailFrom = "bipk eeot jjsm fvqy";
         this.subject = "Hello!";
         this.messageBody = "Hello test email, how are you?";
     }
 
-    public Email(String emailFrom, String passwordEmailFrom, String emailTo, String subject, String messageBody) {
+    /*
+    Aqui nós temos um construtor com todos os campos informados, esse construtor recebe uma lista
+    de emails destinatários  e adiciona esses emails ao atributo de
+    emailsTo (que é a lista de emails destinatários que irá ser passada pro sendEmails)
+    */
+    public Email(String emailFrom, String passwordEmailFrom, List<String> emailsTo, String subject, String messageBody) {
         this.emailFrom = emailFrom;
-        this.passwordEmailFrom = passwordEmailFrom;
-        this.emailTo = emailTo;
+        this.passwordEmailFrom = makePasswordRegular(passwordEmailFrom);
+        this.emailsTo = emailsTo;
         this.subject = subject;
         this.messageBody = messageBody;
     }
 
-    public Email(String emailTo, String subject, String messageBody) {
+    /*
+    Esse construtor serve para quando não for informado um email remetente, vamos utilizar um email como padrão
+    para que a mensagem seja enviada.
+     */
+    public Email(List<String> emailsTo, String subject, String messageBody) {
         this.emailFrom = "emailsendert82@gmail.com";
         this.passwordEmailFrom = "bipk eeot jjsm fvqy";
-        this.emailTo = emailTo;
+        this.emailsTo = emailsTo;
         this.subject = subject;
         this.messageBody = messageBody;
     }
 
+    /*
+    Este método serve para validar a senha esperada pela API (xxxx xxxx xxxx xxxx)
+     */
+    public static String makePasswordRegular(String password) throws IllegalArgumentException {
+        password = password.toLowerCase();
+
+        // Remove todos os caracteres não alfabéticos
+        password = password.replaceAll("[^a-z]", "");
+
+        // Se o comprimento da senha for menor que 16 caracteres, lança uma exceção
+        if (password.length() < 16) {
+            throw new IllegalArgumentException("A senha fornecida é fora do padrao esperado. Espera-se 16 caracteres alfabeticos");
+        }
+
+        // Divide a senha em partes de 4 caracteres
+        StringBuilder regularPassword = new StringBuilder();
+        for (int i = 0; i < password.length(); i += 4) {
+            regularPassword.append(password.substring(i, Math.min(i + 4, password.length())));
+            regularPassword.append(" ");
+        }
+
+        // Remove o espaço extra no final
+        return regularPassword.toString().trim();
+    }
+
+    /*
+    Métodos getters e setters
+     */
     public String getEmailFrom() {
         return emailFrom;
     }
@@ -85,12 +155,12 @@ public class Email {
         this.passwordEmailFrom = passwordEmailFrom;
     }
 
-    public String getEmailTo() {
-        return emailTo;
+    public List<String> getEmailsTo() {
+        return emailsTo;
     }
 
-    public void setEmailTo(String emailTo) {
-        this.emailTo = emailTo;
+    public void setEmailsTo(List<String> emailsTo) {
+        this.emailsTo = emailsTo;
     }
 
     public String getSubject() {
